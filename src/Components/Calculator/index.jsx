0781128801425d8ac3201ae3b6af64eea3e5a534
Milan Fernando <milanburny@gmail.com>
2024-06-28 16:17:30 +0100

    Added functionality to form

--- src/Components/Calculator/index.jsx
+++ src/Components/Calculator/index.jsx
@@ -1,13 +1,42 @@
+import { useState } from "react"
+
 export default function Calculator() {
+    const [bill,setBill] = useState(0)
+    const [people,setPeople] = useState(0)
+    const [tip,setTip] = useState(0)
+    
+    function handleBill(event){
+        setBill(event.target.value)
+    }
+
+    function handlePeople(event){
+        setPeople(event.target.value)
+    }
+
+    function handleTip(event){
+        setTip(event.target.value)
+    }
+
+    function handleSubmit(event){
+        event.preventDefault()
+        console.log((Number(bill)*Number(tip))/100)
+    
+    }
+
     return (
         <form>
             <label htmlFor="Total_Bill">Total Bill</label>
-            <input id="Total_Bill" type="text" /> <br />
+            <input id="Total_Bill" type="number" onChange={handleBill}/> <br />
+
             <label htmlFor="No_of_people">No of people</label>
-            <input id="No_of_people" type="text" /> <br />
+            <input id="No_of_people" type="number" onChange={handlePeople}/> <br />
+
             <label htmlFor="Tip%">Tip %</label>
-            <input id="Tip%" type="text" /> <br />
-            <input type="submit" value="Submit" />
+            <input id="Tip%" type="number" onChange={handleTip}/> <br />
+
+
+            <input type="submit" value="Submit" onClick={handleSubmit} />
+            <p> </p>
         </form>
     )
 }
